34d43e3cc59b5523d8be5075204d2234e3d04b79
giscafer <giscafer@outlook.com>
2018-12-26 17:23:42 +0800

    details

--- problems/009-palindrome-number/index.js
+++ problems/009-palindrome-number/index.js
@@ -1,10 +1,12 @@
 
 /**
  * 题目描述: https://leetcode-cn.com/problems/palindrome-number/
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
  * @param {number} x
  * @return {boolean}
  * 思路：x转为数组处理，拆分为两个分组，然后reverse一个分组再转为字符串做比较。
  */
+
 const isPalindrome = function (x) {
     if (typeof x !== 'number') throw new Error('x must be a number');
     if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
@@ -29,7 +31,7 @@ const isPalindrome = function (x) {
 /**
  * @param {number} x
  * @return {boolean}
- * 解法2，思路：数字本身反转。（因为js中数值计算问题，会存在21.200000000000728的情况，所以不宜采用此方式）
+ * 解法2，思路：数字本身反转。（因为js中数值计算问题，会存在21.200000000000728的情况，还需处理一下，所以不宜采用此方式）
  * https://leetcode-cn.com/problems/palindrome-number/solution/
  */
 const isPalindrome2 = function (x) {
