8b322a0bd6a523ec06d0dc65bd214a8e52c921a2
giscafer <giscafer@outlook.com>
2018-12-26 13:38:52 +0800

    eslint

--- problems/001-two-sum/testcase.js
+++ problems/001-two-sum/testcase.js
@@ -1,30 +1,26 @@
 
-
-
-
 const assert = require('assert');
 const program = require('./index');
 
 const testCases = [
     {
         input: [[2, 7, 11, 15], 9],
-        output: [0, 1],
+        output: [0, 1]
     },
     {
         input: [[3, 2, 4], 6],
-        output: [1, 2],
+        output: [1, 2]
     },
     {
         input: [[0, 4, 3, 0], 0],
-        output: [0, 3],
-    },
+        output: [0, 3]
+    }
 ];
-
 
 describe('001-two-sum', () => {
     it('testcase should be', () => {
         testCases.forEach(testcase => {
             assert.deepEqual(program.apply(null, testcase.input), testcase.output);
-        })
+        });
     });
-})
+});
